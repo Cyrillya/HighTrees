deee76408ae249491b6816bdcd4a8f5d093e5ab1
59193481 <59193481@qq.com>
2022-02-26 09:28:38 +0800

    添加项目文件。

---
+++ HigherTreeSystem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace HighTrees
+{
+    public class HigherTreeSystem : ModSystem
+    {
+        public override void Load() {
+            base.Load();
+            config = ModContent.GetInstance<HigherTreesConfig>();
+            IL.Terraria.WorldGen.GrowTree += new ILContext.Manipulator(WorldGen_GrowTree);
+            // 宝石树高度直接这么改就行了，很方便
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Ruby.TreeHeightMin = config.GemHeightMin;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Ruby.TreeHeightMax = config.GemHeightMax;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Diamond.TreeHeightMin = config.GemHeightMin;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Diamond.TreeHeightMax = config.GemHeightMax;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Topaz.TreeHeightMin = config.GemHeightMin;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Topaz.TreeHeightMax = config.GemHeightMax;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Amethyst.TreeHeightMin = config.GemHeightMin;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Amethyst.TreeHeightMax = config.GemHeightMax;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Sappphire.TreeHeightMin = config.GemHeightMin;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Sappphire.TreeHeightMax = config.GemHeightMax;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Emerald.TreeHeightMin = config.GemHeightMin;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Emerald.TreeHeightMax = config.GemHeightMax;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Amber.TreeHeightMin = config.GemHeightMin;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Amber.TreeHeightMax = config.GemHeightMax;
+            WorldGen.GrowTreeSettings.Profiles.VanityTree_Sakura.TreeHeightMin = config.SakuraHeightMin;
+            WorldGen.GrowTreeSettings.Profiles.VanityTree_Sakura.TreeHeightMax = config.SakuraHeightMax;
+            WorldGen.GrowTreeSettings.Profiles.VanityTree_Willow.TreeHeightMin = config.WillowHeightMin;
+            WorldGen.GrowTreeSettings.Profiles.VanityTree_Willow.TreeHeightMax = config.WillowHeightMax;
+        }
+
+        public override void Unload() {
+            base.Unload();
+            config = null;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Ruby.TreeHeightMin = 7;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Ruby.TreeHeightMax = 12;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Diamond.TreeHeightMin = 7;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Diamond.TreeHeightMax = 12;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Topaz.TreeHeightMin = 7;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Topaz.TreeHeightMax = 12;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Amethyst.TreeHeightMin = 7;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Amethyst.TreeHeightMax = 12;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Sappphire.TreeHeightMin = 7;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Sappphire.TreeHeightMax = 12;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Emerald.TreeHeightMin = 7;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Emerald.TreeHeightMax = 12;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Amber.TreeHeightMin = 7;
+            WorldGen.GrowTreeSettings.Profiles.GemTree_Amber.TreeHeightMax = 12;
+            WorldGen.GrowTreeSettings.Profiles.VanityTree_Sakura.TreeHeightMin = 7;
+            WorldGen.GrowTreeSettings.Profiles.VanityTree_Sakura.TreeHeightMax = 12;
+            WorldGen.GrowTreeSettings.Profiles.VanityTree_Willow.TreeHeightMin = 7;
+            WorldGen.GrowTreeSettings.Profiles.VanityTree_Willow.TreeHeightMax = 12;
+        }
+
+        // 通过简单的IL修改一般树木高度
+        private void WorldGen_GrowTree(ILContext il) {
+            ILCursor c = new ILCursor(il);
+            c.GotoNext(MoveType.After, i => i.MatchLdcI4(5));
+            c.EmitDelegate((int min) => config.MostMin);
+            c.GotoNext(MoveType.After, i => i.MatchLdcI4(17));
+            c.EmitDelegate((int max) => config.MostMax + 1);
+        }
+
+        internal static HigherTreesConfig config;
+    }
+}
---
+++ HigherTreesConfig.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace HighTrees
+{
+	[Label("$Mods.HighTrees.Configs.Label.Title")]
+	public class HigherTreesConfig : ModConfig
+	{
+		public override ConfigScope Mode
+		{
+			get
+			{
+				return ConfigScope.ServerSide;
+			}
+		}
+
+		public override void OnChanged()
+		{
+			base.OnChanged();
+			if (MostMin > MostMax)
+			{
+				MostMin = MostMax;
+			}
+			if (GemHeightMin > GemHeightMax)
+			{
+				GemHeightMin = GemHeightMax;
+			}
+			if (SakuraHeightMin > SakuraHeightMax)
+			{
+				SakuraHeightMin = SakuraHeightMax;
+			}
+			if (WillowHeightMin > WillowHeightMax)
+			{
+				WillowHeightMin = WillowHeightMax;
+			}
+		}
+
+		[Range(1, 1000)]
+		[ReloadRequired]
+		[DefaultValue(5)]
+		[Label("$Mods.HighTrees.Configs.Label.MinMost")]
+		[Tooltip("$Mods.HighTrees.Configs.Tooltip.MinMost")]
+		public int MostMin;
+
+		[Range(1, 1000)]
+		[ReloadRequired]
+		[DefaultValue(16)]
+		[Label("$Mods.HighTrees.Configs.Label.MaxMost")]
+		[Tooltip("$Mods.HighTrees.Configs.Tooltip.MaxMost")]
+		public int MostMax;
+
+		[Range(1, 1000)]
+		[ReloadRequired]
+		[DefaultValue(7)]
+		[Label("$Mods.HighTrees.Configs.Label.MinGem")]
+		[Tooltip("$Mods.HighTrees.Configs.Tooltip.MinGem")]
+		public int GemHeightMin;
+
+		[Range(1, 1000)]
+		[ReloadRequired]
+		[DefaultValue(12)]
+		[Label("$Mods.HighTrees.Configs.Label.MaxGem")]
+		[Tooltip("$Mods.HighTrees.Configs.Tooltip.MaxGem")]
+		public int GemHeightMax;
+
+		[Range(1, 1000)]
+		[ReloadRequired]
+		[DefaultValue(7)]
+		[Label("$Mods.HighTrees.Configs.Label.MinSakura")]
+		[Tooltip("$Mods.HighTrees.Configs.Tooltip.MinSakura")]
+		public int SakuraHeightMin;
+
+		[Range(1, 1000)]
+		[ReloadRequired]
+		[DefaultValue(12)]
+		[Label("$Mods.HighTrees.Configs.Label.MaxSakura")]
+		[Tooltip("$Mods.HighTrees.Configs.Tooltip.MaxSakura")]
+		public int SakuraHeightMax;
+
+		[Range(1, 1000)]
+		[ReloadRequired]
+		[DefaultValue(7)]
+		[Label("$Mods.HighTrees.Configs.Label.MinWillow")]
+		[Tooltip("$Mods.HighTrees.Configs.Tooltip.MinWillow")]
+		public int WillowHeightMin;
+
+		[Range(1, 1000)]
+		[ReloadRequired]
+		[DefaultValue(12)]
+		[Label("$Mods.HighTrees.Configs.Label.MaxWillow")]
+		[Tooltip("$Mods.HighTrees.Configs.Tooltip.MaxWillow")]
+		public int WillowHeightMax;
+	}
+}
